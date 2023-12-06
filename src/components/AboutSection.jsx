80dde012865b6f8a4e2e745b4469abad934b5d10
medievalbullet <reznagykirill@gmail.com>
2023-12-06 19:56:08 +0100

    About section styling improvment

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -15,14 +15,14 @@ export default function AboutSection() {
   return (
     <section id='about-container'>
         <div id='about-main'>
-          <h2 className='slide-in-element' id='about-title'>ABOUT.</h2>
+          <h2 className='slide-in-element' id='about-header'>ABOUT ME</h2>
           <div id='about-content'>
             <section className='about-section'>
-              <h3 className='about-titles-group slide-in-element' id='about-journey'>My Journey</h3>
+              <h3 className='about-title slide-in-element' id='about-journey'><i class="material-icons about-icon">fork_right</i>My Journey</h3>
               <p className='about-text slide-in-element' id='about-journey-text'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
             </section>
             <section className='about-section'>
-              <h3 className='about-titles-group slide-in-element' id='about-journey-title'>Hobbies & Activities</h3>
+              <h3 className='about-title slide-in-element' id='about-journey-title'><i class="material-icons about-icon">sports_esports</i>Hobbies & Activities</h3>
               <p className='about-text slide-in-element' id='about-journey-text'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
             </section>
           </div>
